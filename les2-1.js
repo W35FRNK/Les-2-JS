427f4cf585ecba4c550592a9844decba9246477f
Frank Wesdijk <frank.wesdijk@gmail.com>
2023-01-16 11:36:41 +0100

    les2 JS

---
+++ les2-1.js
@@ -0,0 +1,30 @@
+
+
+  const naam = prompt('Uw naam', 'Naam');
+  const geboortejaar = prompt('Uw geboortejaar', 'Geboortejaar' );
+  const tekst = 'U heet ' + naam + ' en uw geboortejaar is ' + geboortejaar;
+
+  document.getElementById('welkom').innerHTML = tekst;
+
+
+  const huidigjaar = 2023;
+  const totaal = parseInt(huidigjaar) - parseInt(geboortejaar)
+  const tekst2 = 'De leeftijd van uw persoon is ' + totaal;
+
+  document.getElementById('leeftijd').innerHTML = tekst2;
+
+  const lengteZin = tekst2.length;
+  console.log(lengteZin);
+  console.log(tekst2.indexOf('n'));
+
+
+  const x = parseInt(geboortejaar);
+  if (x === 1980) {
+    console.log('true');
+  }
+
+  else {
+    console.log('false');
+  }
+
+
